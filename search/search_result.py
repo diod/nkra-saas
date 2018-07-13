c37f17248cb575e8bac4a6711adf2da99ffbbe28
yaklyushkin <armmv@mail.ru>
2018-07-13 09:33:16 +0300

    Fix pdf link show for Syntax corpus.

--- search/search_result.py
+++ search/search_result.py
@@ -48,7 +48,7 @@ class SearchResult(object):
         :param subcorpus:
 
         """
-        if subcorpus is not None:
+        if not (subcorpus is None):
             query += subcorpus
         query = self._process_query(query, docid)
         params = self._get_params(query, kps, grouping, group_attr, max_docs,
