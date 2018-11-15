ca906d4d96e4177fc5d615c715e6ac9a2c9ab128
Dmitry Vylegzhanin <diod@psax.ru>
2018-11-15 21:25:09 +0300

    server.py: rewrite to flask+uwsgi

--- server.py
+++ server.py
@@ -6,11 +6,12 @@ import os
 import logging
 import argparse
 import urlparse
-import BaseHTTPServer
 import StringIO
 
 import requests
 
+from flask import Flask, current_app, redirect, request, Response
+from flask_classful import FlaskView, route
 from cgi import parse_header, parse_multipart
 
 from search import search
@@ -23,101 +24,78 @@ logging.basicConfig(format='%(asctime)s %(message)s',
                     level=logging.DEBUG)
 
 
-def write_pid_file():
-    pid = str(os.getpid())
-    f = open('/var/run/saas.pid', 'w')
-    f.write(pid)
-    f.close()
 
-
-class ServerHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+class ServerHandler(FlaskView):
     engine = search.SearchEngine()
 
-    def do_GET(self):
-        if self.path.startswith('/favicon'):
-            with open('www/favicon.ico', 'rb') as infile:
-                self.wfile.write(infile.read())
+    @route('/favicon.ico', methods=['GET'])
+    def static_favicon(self):
+        return application.send_static_file('favicon.ico');
 
-        elif self.path.startswith('/www/js/graphic.js'):
-            with open('www/js/graphic/graphic.js', 'r') as infile:
-                self.wfile.write(infile.read())
+    @route('/www/js/graphic.js', methods=['GET'])
+    def static_graphic(self):
+        return application.send_static_file('js/graphic/graphic.js');
 
-        else:
+    @route('/video.xml', methods=['GET'])
+    def get_video(self):
+        redirect_url = "http://streaming.video.yandex.ru/get/ruscorpora-video/%s/sq.mp4" % request.args.get("id", "none")
+        return redirect(redirect_url, 301);
+
+    @route('/<path:path>', methods=['GET'])
+    def search(self,path):
             args = ServerHandler.args
             output = StringIO.StringIO()
-            query = urlparse.urlparse(self.path).query
+
+            #raw query string (non utf-8)
+            query = request.query_string
             params = urlparse.parse_qs(query)
 
-            if params.get('mode', [None])[0] == 'syntax':
+            if params.get('mode', None) == 'syntax':
                 params = urlparse.parse_qs(query, keep_blank_values=True)
 
-            if "video.xml" in self.path:
-                redirect_url = "http://streaming.video.yandex.ru/get/ruscorpora-video/%s/sq.mp4" % params.get("id", ["None"])[0]
-                self.send_response(301)
-                self.send_header("Location", redirect_url)
-                self.send_header("Content-Type", "application/octet-stream")
-                self.end_headers()
-                return
-
-            self.send_response(200, 'OK')
             if args.rendering == 'xml':
-                self.send_header('Content-Type', 'text/xml; charset=utf-8')
-                self.end_headers()
+                content_type = 'text/xml; charset=utf-8'
                 output.write('<?xml version=\'1.0\' encoding=\'utf-8\'?>\n')
             elif args.rendering == 'xslt':
-                self.send_header('Content-Type', 'text/html; charset=utf-8')
-                self.end_headers()
+                content_type = 'text/html; charset=utf-8'
 
             self.engine.search(params, output, args)
             result = output.getvalue()
+
             if args.rendering == 'xslt':
                 result = xslt.transform(result, params)
-            self.wfile.write(result)
 
-    def do_POST(self):
-        if self.path.startswith('/bug-report.xml'):
-            params = self.parse_POST()
-            msg = params.get('msg', ['<Message not found>'])
-            url = params.get('url', ['<Url not found>'])
+            result = xslt.tostring(result, encoding='utf-8')
+            return Response(result, content_type=content_type);
 
-            ruscopora_bug_reporter.process_bug_report(msg[0], url[0]);
+    @route('/bug-report.xml', methods=['POST'])
+    def bugreport(self):
 
-            self.wfile.write("Ok")
+        params = request.form
+        msg = params.get('msg', '<Message not found>');
+        url = params.get('url', '<Url not found>');
 
-    def parse_POST(self):
-        if 'content-type' in self.headers:
-            ctype, pdict = parse_header(self.headers['content-type'])
-            if ctype == 'multipart/form-data':
-                postvars = parse_multipart(self.rfile, pdict)
-            elif ctype == 'application/x-www-form-urlencoded':
-                length = int(self.headers['content-length'])
-                postvars = urlparse.parse_qs(
-                        self.rfile.read(length),
-                        keep_blank_values=1)
-            else:
-                postvars = {}
-        else:
-            length = int(self.headers['content-length'])
-            postvars = urlparse.parse_qs(
-                self.rfile.read(length),
-                keep_blank_values=1)
-        return postvars
+        ruscopora_bug_reporter.process_bug_report(msg, url);
+        return Response("Ok\n", content_type="text/plain; charset=UTF-8")
 
-def main():
-    write_pid_file()
-    parser = argparse.ArgumentParser(description='Starts python web server.')
-    parser.add_argument('--subcorpus', default='main')
-    parser.add_argument('--kps', type=int, default=1, nargs='?')
-    parser.add_argument('--rendering', default='xslt',
+
+############################################
+#
+# Init application for WSGI
+#
+
+parser = argparse.ArgumentParser(description='Starts python web server.')
+parser.add_argument('--subcorpus', default='main')
+parser.add_argument('--kps', type=int, default=1, nargs='?')
+parser.add_argument('--rendering', default='xslt',
                         choices=['xml', 'xslt'], nargs='?')
-    parser.add_argument('-p', '--port', type=int, default=8001, nargs='?')
-    args = parser.parse_args()
-    ServerHandler.args = args
-    serv = BaseHTTPServer.HTTPServer(('', args.port), ServerHandler)
-    while True:
-        serv.handle_request()
-    serv.server_close()
+parser.add_argument('-p', '--port', type=int, default=8001, nargs='?')
+args = parser.parse_args()
 
+application = Flask(__name__, static_folder='www', static_url_path='/www')
+
+ServerHandler.args = args
+ServerHandler.register(application, route_base="/")
 
 if __name__ == '__main__':
-    main()
+    application.run(host="0.0.0.0", port=args.port)
